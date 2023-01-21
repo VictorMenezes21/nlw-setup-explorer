15a0e968f704999cab54ffd75ce76c54d226add1
Victor <victorm_oliv@hotmail.com>
2023-01-21 16:07:59 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const form = document.querySelector("#form-habits")
+const nlwSetup = new NLWSetup(form)
+const button = document.querySelector('header button')
+
+button.addEventListener('click', add)
+form.addEventListener('change', save)
+
+function add() {
+
+  const today = new Date().toLocaleDateString('pt-br').slice(0, -5)
+  // const today = '01-01'
+  const dayExists = nlwSetup.dayExists(today)
+
+  if (dayExists) {
+    alert('Dia já incluso ❌')
+    return
+  }
+
+  alert('Adicionado com sucesso ✔')
+  nlwSetup.addDay(today)
+}
+
+function save() {
+  localStorage.setItem('NLWSetup@habits', JSON.stringify(nlwSetup.data))
+}
+
+// const data = {
+//   run: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
+//   takePills: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
+//   journal: ["01-01", "01-02", "01-03", "01-04", "01-05", "01-06", "01-07"],
+// }
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
+nlwSetup.setData(data) 
+nlwSetup.load()
